5d0dbc51eab71a076008dbc32fb83a81d7cf723c
Vo Minh Tuan <0306221191@caothang.edu.vn>
2025-01-03 10:25:42 +0700

    thêm file asset và ảnh background

--- lib/DangNhap.dart
+++ lib/DangNhap.dart
@@ -9,56 +9,55 @@ class DangNhapState extends State<DangNhap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Fruit Paradise",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+      
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/nen.jpg'), // Đường dẫn đến ảnh nền trong thư mục assets
+            fit: BoxFit.cover, // Căng đầy màn hình
           ),
         ),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0), // Padding xung quanh toàn bộ Column
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Đăng nhập",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 50),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0), // Padding cho TextField
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    border: OutlineInputBorder(),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0), // Padding xung quanh toàn bộ Column
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Đăng nhập",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green, // Màu chữ trắng để nổi bật trên nền
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0), // Padding cho TextField
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Mật khẩu",
-                    border: OutlineInputBorder(),
+                const SizedBox(height: 50),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0), // Padding cho TextField
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Đăng nhập"),
-              ),
-            ],
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0), // Padding cho TextField
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Mật khẩu",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Đăng nhập"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
